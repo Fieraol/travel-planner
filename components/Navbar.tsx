01d2f40c1548cece14688435795f9bf9402b5a45
Fieraol <fierashiferaw@gmail.com>
2025-09-02 12:27:04 -0400

    prisma/auth added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { login, logout } from "@/lib/auth-actions";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -21,7 +24,10 @@ export default function Navbar() {
             Globe
           </Link>
 
-          <button className="flex items-center justify-center bg-gray-800 hover:bg-gray-900 text-white p-2 rounded-sm cursor-pointer">
+          <button
+            className="flex items-center justify-center bg-gray-800 hover:bg-gray-900 text-white p-2 rounded-sm cursor-pointer"
+            onClick={login}
+          >
             Sign In
             <svg
               className="w-6 h-6 ml-2"
